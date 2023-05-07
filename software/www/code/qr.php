bc35bf8a114b7cef234a50a113a9db3a2fdb9894
Peter Otte <pbotte@uni-mainz.de>
2023-05-07 19:15:01 +0200

    Rechnungen als PDF via Webserver

---
+++ software/www/code/qr.php
@@ -0,0 +1,20 @@
+<?php
+    include "qr/lib/full/qrlib.php";
+ 
+    $errorCorrectionLevel = 'M';
+    if (isset($_GET['level']) && in_array($_GET['level'], array('L','M','Q','H')))
+        $errorCorrectionLevel = $_GET['level'];
+ 
+    $matrixPointSize = 4;
+    if (isset($_GET['size']))
+        $matrixPointSize = min(max((int)$_GET['size'], 1), 10);
+
+    $textData = "no data";
+ 
+    if (isset($_GET['t']) && (trim($_GET['t']) != '')) {
+        $textData = $_GET['t'];
+    }
+    
+//    print($textData); 
+    QRcode::png($textData, false, $errorCorrectionLevel, $matrixPointSize, 4); // Die letzte 4 ist die Dicke des weißen Rahmens
+?>
