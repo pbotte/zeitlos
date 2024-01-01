70558a2cb335310c97ee35f7e22f55ccf6e69650
pbotte <github.com@imsteinert.de>
2024-01-01 12:26:00 +0100

    Prepare for >8 scales per controller

--- software/shelf-controller/shelf-controller.py
+++ software/shelf-controller/shelf-controller.py
@@ -260,6 +260,7 @@ def search_waagen():
             logger.debug(f"Gesamt gelesen: {r=}")
             v=struct.unpack('<f',bytearray(r))[0]  #unpack returns: (-524945,), get the right value with [0]
             waagen[neue_i2c_adresse]['zero'] = v
+            client.publish(f"homie/{args.mqtt_client_name}/scales/{waagen[neue_i2c_adresse]['mac']}/zero_raw", v, qos=0, retain=True)
 
 
             #Read out slope from scale: 4 single bytes (address 1..4)
@@ -274,6 +275,7 @@ def search_waagen():
             v=struct.unpack('<f',bytearray(r))[0]  #unpack returns: (-7.092198939062655e-05,), get the right value with [0]
             if math.isnan(v): v=-4.632391446259352e-05 #set to default value for 4*50kg scales if value is not set (==nan)
             waagen[neue_i2c_adresse]['slope'] = v
+            client.publish(f"homie/{args.mqtt_client_name}/scales/{waagen[neue_i2c_adresse]['mac']}/slope", v, qos=0, retain=True)
 
 
             send_and_recv(f"w{neue_i2c_adresse:02X}00") #set back normal read mode
@@ -285,7 +287,12 @@ def search_waagen():
             #individuelle LED wieder aus
             #res3 = send_and_recv(f"w{neue_i2c_adresse:02X}02")
             #logger.info(f"Rückgabewert Schreiben I2C LED aus: {res3}")
-            #time.sleep(0.1)
+            
+            if anzahl_waagen % 8==0: #nicht mehr als 8 LEDS gleichzeitig an, da Strom für Controller zu hoch
+              time.sleep(0.5)
+              #alle LEDs aus
+              send_and_recv("w0004")
+              logger.info(f"Switch off all LEDs to limit current drawn from shelf controller. This is repeated every 8 found scales.")
 
     logger.info(f"gefundene Waagen Anzahl: {anzahl_waagen}")
     logger.info(f"gefundene Waagen: {waagen}")
@@ -349,6 +356,7 @@ while True:
                 for i,x in enumerate(v):
                     res6 = send_and_recv(f"w{w[0]:02X}05{i+5:02X}{x:02X}") # write 1 byte to address i+5
                     logger.info(f"Write Return (address {i+5}, value: {x}) {res6=}")
+                client.publish(f"homie/{args.mqtt_client_name}/scales/{w[1]['mac']}/zero_raw", waagen[w[0]]['zero'], qos=0, retain=True)
 
 
         if len(msplit) == 6 and msplit[2].lower() == "cmd" and msplit[3].lower() == "scales" and msplit[5].lower() == "set_slope":
@@ -368,7 +376,7 @@ while True:
                     for i,x in enumerate(v):
                         res6 = send_and_recv(f"w{i2c_address:02X}05{i+1:02X}{x:02X}") # write 1 byte to address i+1
                         logger.info(f"Write Return {res6=}")
-
+                    client.publish(f"homie/{args.mqtt_client_name}/scales/{waagen[i2c_address]['mac']}/slope", waagen[i2c_address]['slope'], qos=0, retain=True)
                 else:
                     logger.warning(f"Value passed: '{m}' is no float.")
             else:
