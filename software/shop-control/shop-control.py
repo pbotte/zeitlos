a6fffe8d94c4e65abd5fff2aa5a9410b180216ad
Peter Otte <pbotte@uni-mainz.de>
2024-04-19 23:46:00 +0200

    Produktezuordnung Waagen setzen

--- software/shop-control/shop-control.py
+++ software/shop-control/shop-control.py
@@ -82,6 +82,9 @@ def on_connect(client, userdata, flags, rc):
         client.subscribe("homie/public_webpage_supplier/+/cmd/#")
         client.subscribe("homie/door/#")
         client.subscribe("homie/cardreader/#")
+        client.subscribe("homie/+/scales/+/touched") #zum Waagen auswählen beim Bestückenu und Kalibrieren
+        client.subscribe("homie/"+mqtt_client_name+"/last_touched/reset") #um wieder neue Waagen auswählen zu können
+        client.subscribe("homie/"+mqtt_client_name+"/last_touched/set_product_id") #Neues Produkt auf Waage setzen
 
         logger.debug("MQTT: Subscribed to all topics")
     else:
@@ -141,6 +144,7 @@ shop_status_last_change_timestamp = time.time()
 def set_shop_status(v):
     global shop_status
     global shop_status_last_change_timestamp
+    global status_last_touched_shelf_str, status_last_touched_scale_str
     if shop_status == v:
         return
     shop_status = v
@@ -150,6 +154,15 @@ def set_shop_status(v):
     client.publish("homie/"+mqtt_client_name+"/shop_status", shop_status, qos=1, retain=True)
     client.publish("homie/"+mqtt_client_name+"/shop_status_last_change_timestamp", shop_status_last_change_timestamp, qos=1, retain=True)
 
+    #Wenn Status gewechelt wird in einen shop_status != 18, dann sollen die Informationen über die ausgewählte Waage gelöscht werden und LED wieder ausgeschaltet
+    if shop_status != 18 and status_last_touched_shelf_str and status_last_touched_scale_str: 
+      client.publish(f"homie/{status_last_touched_shelf_str}/cmd/scales/{status_last_touched_scale_str.upper()}/led", "0", qos=1, retain=False)
+      status_last_touched_shelf_str = ""
+      status_last_touched_scale_str = ""
+      client.publish("homie/"+mqtt_client_name+"/last_touched/shelf_str", status_last_touched_shelf_str, qos=1, retain=True) #zurücksetzen
+      client.publish("homie/"+mqtt_client_name+"/last_touched/scale_str", status_last_touched_scale_str, qos=1, retain=True) #zurücksetzen
+      client.publish("homie/"+mqtt_client_name+"/last_touched/product_id", status_last_touched_scale_str, qos=1, retain=True) #zurücksetzen
+
 
 cardreader_busy = False # set by MQTT messages from cardreader. When true, do not submit any task to cardreader and simply wait
 cardreader_last_textblock = "" # MQTT messages from cardread typically in receipe style from homie/cardreader/text_block
@@ -157,6 +170,10 @@ cardreader_last_textblock = "" # MQTT messages from cardread typically in receip
 actBasket = {"data": {}, "total": 0, "products_count": 0}
 status_no_person_in_shop = None # True if homie/shop-track-collector/pixels-above-reference == 0, else False
 status_door_closed = None
+
+# zum Abspeichern von Informationen aus: homie/scale-shop-shelf06-0-1.2.5.5-1.0/scales/4930370A3419/touched
+status_last_touched_shelf_str = "" #scale-shop-shelf06-0-1.2.5.5-1.0
+status_last_touched_scale_str = "" #4930370A3419
 
 mqtt_queue=queue.Queue()
 def on_message(client, userdata, message):
@@ -241,16 +258,105 @@ while loop_var:
         
         #Fernsteuerung durch supplier_full.php
         if len(msplit) == 5 and msplit[1].lower() == "public_webpage_supplier" and msplit[3].lower() == "cmd" and msplit[4].lower() == "open_door":
-          if not shop_status in (8,): #Bei technischen Fehler soll diese Funktion nicht gegeben sein.
+          if not shop_status in (8,19,): #Bei technischen Fehler soll diese Funktion nicht gegeben sein. Ebenfalls bei techn. Wartung nicht.
             logger.info(f"Türöffner aktiviert durch supplier_full.php mit MQTT-topic: {message.topic.lower()}")
             client.publish("homie/fsr-control/innen/tuerschliesser/set", '1', qos=2, retain=False)  # send door open impuls
             client.publish("homie/fsr-control/innen/licht/set", '1', qos=1, retain=False)  # Licht an
           else:
-            logger.warning(f"Fehler: Türöffner durch supplier_full.php mit MQTT-topic: {message.topic.lower()}. Kann jedoch nict durchgeführt werden, da shop_status: {shop_status}")
+            logger.warning(f"Fehler: Türöffner durch supplier_full.php mit MQTT-topic: {message.topic.lower()}. Kann jedoch nicht durchgeführt werden, da shop_status: {shop_status}")
+        if len(msplit) == 5 and msplit[1].lower() == "public_webpage_supplier" and msplit[3].lower() == "cmd" and msplit[4].lower() == "close_shop":
+          if shop_status in (0,1,7,13,16,6,18,9): #nur wenn auf Kunden gewartet wird, darf geschlossen werden, oder beim/nach dem Einräumen und Kundenhilfe
+            set_shop_status(10)
+            logger.info(f"Laden wurde geschlossen durch supplier_full.php mit MQTT-topic: {message.topic.lower()}")
+            client.publish("homie/fsr-control/innen/licht/set", '0', qos=1, retain=False)  # Licht aus
+          else:
+            logger.warning(f"Fehler: Laden schlie0en durch supplier_full.php mit MQTT-topic: {message.topic.lower()}. Kann jedoch nicht durchgeführt werden, da shop_status: {shop_status}")
+        if len(msplit) == 5 and msplit[1].lower() == "public_webpage_supplier" and msplit[3].lower() == "cmd" and msplit[4].lower() == "open_shop":
+          if shop_status in (6,18,10,9): #nur nach dem Einräumen und wenn Laden geschlossen und Kundenhilfe
+            set_shop_status(0)
+            client.publish("homie/display-power-control-shop-door/power/set", '1', qos=1, retain=False) #schalte das Display außen ein
+            logger.info(f"Laden wurde geöffnet durch supplier_full.php mit MQTT-topic: {message.topic.lower()}")
+          else:
+            logger.warning(f"Fehler: Laden öffnen durch supplier_full.php mit MQTT-topic: {message.topic.lower()}. Kann jedoch nicht durchgeführt werden, da shop_status: {shop_status}")
+        if len(msplit) == 5 and msplit[1].lower() == "public_webpage_supplier" and msplit[3].lower() == "cmd" and msplit[4].lower() == "start_assign":
+          if not shop_status in (8,19): #Bei technischen Fehler soll diese Funktion nicht gegeben sein. Ebenfalls bei techn. Wartung nicht
+            set_shop_status(6)
+            logger.info(f"Produktzuordnung durch supplier_full.php mit MQTT-topic: {message.topic.lower()}")
+          else:
+            logger.warning(f"Fehler: Produktzuordnung durch supplier_full.php mit MQTT-topic: {message.topic.lower()}. Kann jedoch nicht durchgeführt werden, da shop_status: {shop_status}")
 
+        #request update from db
         if len(msplit) == 3 and msplit[2].lower() == "update_data_from_db":
            get_all_data_from_db()
            send_basket_products_scales_to_mqtt()
+
+        #eine Waage wurde gedrückt, Empfang von Nachrichten wie: homie/scale-shop-shelf06-0-1.2.5.5-1.0/scales/4930370A3419/touched
+        if len(msplit) == 5 and msplit[2].lower() == "scales" and msplit[4].lower() == "touched":
+          if shop_status == 6: #Einräum-Modus aktiv, noch keine Waage ausgewählt
+            if m == "1":
+              status_last_touched_shelf_str = msplit[1].lower() #scale-shop-shelf06-0-1.2.5.5-1.0
+              status_last_touched_scale_str = msplit[3].lower() #4930370A3419
+              logger.info(f"Auf dem Regal {status_last_touched_shelf_str} wurde die folgende Waage gedrückt: {status_last_touched_scale_str}")
+              client.publish("homie/"+mqtt_client_name+"/last_touched/shelf_str", status_last_touched_shelf_str, qos=1, retain=True)
+              client.publish("homie/"+mqtt_client_name+"/last_touched/scale_str", status_last_touched_scale_str, qos=1, retain=True)
+
+              logger.info(f"Regal {status_last_touched_shelf_str} mit Waage {status_last_touched_scale_str} ausgewält und LED wird eingeschaltet.")
+              client.publish(f"homie/{status_last_touched_shelf_str}/cmd/scales/{status_last_touched_scale_str.upper()}/led", "1", qos=1, retain=False)
+
+              #produkt-waagen-zuordnung nachschlagen
+              if status_last_touched_scale_str in scales_products:
+                client.publish("homie/"+mqtt_client_name+"/last_touched/product_id", scales_products[status_last_touched_scale_str], qos=1, retain=True)
+
+              set_shop_status(18)
+
+        if message.topic.lower() == "homie/"+mqtt_client_name+"/last_touched/reset":
+          if shop_status==18:
+            set_shop_status(6) #reset der LED und der Variablen werden in dieser Routine durchgeführt
+          else:
+            logger.warning(f"Reset von last_touched nicht ausgeführt, da shop_status nicht == 18.")
+
+        if ( (message.topic.lower() == "homie/"+mqtt_client_name+"/last_touched/set_product_id") or 
+          (len(msplit) == 5 and msplit[1].lower() == "public_webpage_supplier" and msplit[3].lower() == "cmd" and msplit[4].lower() == "assign_product") ):
+          if status_last_touched_shelf_str and status_last_touched_scale_str and shop_status == 18:
+            if m.isdigit():
+              m = int(m)
+            else: 
+              m = None
+            
+            db_prepare()
+            #Bisherigen Eintrag löschen
+            try:
+              sql_str = "DELETE FROM `Products_Scales` WHERE `ScaleID` LIKE ?; "
+              logger.info(f"Execute the following SQL Str: {sql_str} with ({status_last_touched_scale_str})")
+              cur.execute(sql_str, (status_last_touched_scale_str, )) #the last comma here is super important, if only one elem provided
+              conn.commit()
+            except mariadb.Error as e:
+              logger.warning(f"Error while SQL DELETE: {e}")
+
+            #neue Produktzuordnung eintragen, nur wenn Zahl übergeben ist
+            if m:
+              try:
+                sql_str = "INSERT INTO`Products_Scales`(`ProductID`, `ScaleID`) VALUES(?, ? ); "
+                logger.info(f"Execute the following SQL Str: {sql_str} with ({int(m)}, {status_last_touched_scale_str})")
+                cur.execute(sql_str, (int(m), status_last_touched_scale_str, )) #the last comma here is super important, if only one elem provided
+                conn.commit()
+                last_product_scale_id_inserted = cur.lastrowid
+                logger.info(f"Last Inserted ID into Products_Scales: {last_product_scale_id_inserted}")
+              except mariadb.Error as e:
+                logger.warning(f"Error while SQL INSERT: {e}")
+            else:
+               logger.info(f"Waagenzuordnung wurde nur gelöscht und kein neuer Eintrag getätigt.")
+            db_close()
+
+            #Aktuelel Daten von DB einlesen und versenden
+            logger.info("Lese die DB neu ein und verschicke sie per MQTT")
+            get_all_data_from_db()
+            send_basket_products_scales_to_mqtt()
+
+            set_shop_status(6) #reset der LED und der Variablen werden in dieser Routine durchgeführt
+          else:
+            logger.warning(f"Produkt konnte nicht gesetzt werden, da die Variablen nicht gefüllt sind oder weil shop_status nicht 18 ist: {status_last_touched_shelf_str=} {status_last_touched_scale_str=} {shop_status=}")
+
 
         # tracker information to know immediate person presence
         if len(msplit) == 3 and msplit[1].lower() == "shop-track-collector" and msplit[2].lower() == "pixels-above-reference":
@@ -414,8 +520,8 @@ while loop_var:
           next_shop_status = 12
     elif shop_status == 5: #"Kassenbonanzeige
         pass # geht über timout weiter zum nächsten Zustand
-    elif shop_status == 6: # ungenutzter Zustand
-        next_shop_status = 7
+    elif shop_status == 6: # Einräumen durch Betreiber
+        pass
     elif shop_status == 7: #"Warten auf: Vorbereitung für nächsten Kunden"
         cardreader_last_textblock = "" # this typically stores receipes from the card terminal, clear it for new customer
         scales_mass_reference = copy.deepcopy(scales_mass_actual) #Warenkorb zurücksetzen
@@ -487,6 +593,8 @@ while loop_var:
         logger.warning(f"Kein Pre-Auth an Kartenlesegerät gesendet, da {cardreader_busy=}. Warten.")
     elif shop_status == 17: # Warten auf: Kartenterminal Buchung erfolgreich
       pass # Rückmeldung Kartenlesegerät über MQTT-Message
+    elif shop_status == 18: # Einräumen durch Betreiber, Waage ausgewählt
+       pass
     else:
       logger.error(f"Unbekannter {shop_status=} Wert.")
 
